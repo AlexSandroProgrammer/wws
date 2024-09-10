b77fc784848f20ecf86cca62159cf67d9ac97cf9
alexsandro2005 <luisalejandrm533@gmail.com>
2024-09-10 09:05:31 -0500

    feat: Config Sidebar

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -287,87 +287,59 @@
                 <div class="col-md-12 grid-margin">
                     <div class="row">
                         <div class="col-12 col-xl-8 mb-4 mb-xl-0">
-                            <h3 class="font-weight-bold">Welcome Aamir</h3>
-                            <h6 class="font-weight-normal mb-0">All systems are running smoothly! You have <span
-                                    class="text-primary">3 unread alerts!</span></h6>
+                            <h3 class="font-weight-bold">Bienvenido Administrador</h3>
+                            <h6 class="font-weight-normal mb-0">En este panel podras gestionar toda la informacion de
+                                tus tiendas.</h6>
                         </div>
-                        <div class="col-12 col-xl-4">
-                            <div class="justify-content-end d-flex">
-                                <div class="dropdown flex-md-grow-1 flex-xl-grow-0">
-                                    <button class="btn btn-sm btn-light bg-white dropdown-toggle" type="button"
-                                        id="dropdownMenuDate2" data-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="true">
-                                        <i class="mdi mdi-calendar"></i> Today (10 Jan 2021)
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuDate2">
-                                        <a class="dropdown-item" href="#">January - March</a>
-                                        <a class="dropdown-item" href="#">March - June</a>
-                                        <a class="dropdown-item" href="#">June - August</a>
-                                        <a class="dropdown-item" href="#">August - November</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-md-6 grid-margin stretch-card">
                     <div class="card tale-bg">
-                        <div class="card-people mt-auto">
-                            <img src="<?= base_url("images/auth") ?>" alt="people">
-                            <div class="weather-info">
-                                <div class="d-flex">
-                                    <div>
-                                        <h2 class="mb-0 font-weight-normal"><i class="icon-sun mr-2"></i>31<sup>C</sup>
-                                        </h2>
-                                    </div>
-                                    <div class="ml-2">
-                                        <h4 class="location font-weight-normal">Bangalore</h4>
-                                        <h6 class="font-weight-normal">India</h6>
-                                    </div>
-                                </div>
-                            </div>
+                        <div class=" m-auto">
+                            <img src="<?= base_url("images/auth/slide_2.svg") ?>" width="300" alt="people">
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6 grid-margin transparent">
                     <div class="row">
-                        <div class="col-md-6 mb-4 stretch-card transparent">
-                            <div class="card card-tale">
+                        <div class="col-xl-6 mb-4 stretch-card transparent">
+                            <div class="card card-inverse-primary">
                                 <div class="card-body">
-                                    <p class="mb-4">Today’s Bookings</p>
-                                    <p class="fs-30 mb-2">4006</p>
-                                    <p>10.00% (30 days)</p>
+                                    <h4 class="mb-4 fs-40 font-weight-bold">Reparaciones Pendientes</h4>
+                                    <p class="fs-30 mb-2">206</p>
+                                    <p>Cantidad</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6 mb-4 stretch-card transparent">
-                            <div class="card card-dark-blue">
+                        <div class="col-xl-6 mb-4 stretch-card transparent">
+                            <div class="card card-inverse-success">
                                 <div class="card-body">
-                                    <p class="mb-4">Total Bookings</p>
-                                    <p class="fs-30 mb-2">61344</p>
-                                    <p>22.00% (30 days)</p>
+                                    <h4 class="mb-4 fs-40 font-weight-bold">Reparaciones Finalizadas</h4>
+                                    <p class="fs-30 mb-2">400</p>
+                                    <p>Cantidad</p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-6 mb-4 mb-lg-0 stretch-card transparent">
-                            <div class="card card-light-blue">
+                        <div class="col-xl-6 mb-4 mb-lg-0 stretch-card transparent">
+                            <div class="card card-inverse-dark">
                                 <div class="card-body">
-                                    <p class="mb-4">Number of Meetings</p>
-                                    <p class="fs-30 mb-2">34040</p>
-                                    <p>2.00% (30 days)</p>
+                                    <h4 class="mb-4 fs-40 font-weight-bold">Empresas Registradas</h4>
+                                    <p class="fs-30 mb-2">100</p>
+                                    <p>Cantidad</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6 stretch-card transparent">
-                            <div class="card card-light-danger">
+                        <div class="col-xl-6 stretch-card transparent">
+                            <div class="card card-inverse-info">
                                 <div class="card-body">
-                                    <p class="mb-4">Number of Clients</p>
-                                    <p class="fs-30 mb-2">47033</p>
-                                    <p>0.22% (30 days)</p>
+                                    <h4 class="mb-4 fs-40 font-weight-bold">Clientes Registrados</h4>
+                                    <p class="fs-30 mb-2">200</p>
+                                    <p>Cantidad</p>
                                 </div>
                             </div>
                         </div>
